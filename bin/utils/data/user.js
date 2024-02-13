dd97aad5e312691d36096c1a646e3926d2630e40
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-02-12 22:35:05 -0500

    Create User schema

---
+++ bin/utils/data/user.js
@@ -0,0 +1,35 @@
+const bcrypt = require( "bcrypt");
+const mongoose = require("./mongodb");
+
+const UserSchema = new mongoose.Schema({
+    emailAddress: {
+        type: String,
+        unique: true
+    },
+    username: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        set(val) { return bcrypt.hashSync(val, 10) },
+        select: false
+    },
+    DOBYear: {
+        type: Number
+    },
+    DOBMonth: {
+        type: Number
+    },
+    DOBDay: {
+        type: Number
+    },
+    createTime: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+// Create a model from the schema.
+const User = mongoose.model("User", UserSchema);
+module.exports = { User };
