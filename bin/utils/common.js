521fdc203d4a283424513c1fe3a5c2acd201db95
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-02-12 22:35:26 -0500

    Modify to contain common functions

--- bin/utils/common.js
+++ bin/utils/common.js
@@ -1,30 +1,16 @@
-const fs = require('fs/promises');
-const path = require('path');
-const { promisify } = require('util');
+const MonthToNumber = {
+    "January": "01",
+    "February": "02",
+    "March": "03",
+    "April": "04",
+    "May": "05",
+    "June": "06",
+    "July": "07",
+    "August": "08",
+    "September": "09",
+    "October": "10",
+    "November": "11",
+    "December": "12"
+};
 
-/**
- * Get all data from JSON file.
- * @param fileName
- * @returns {Promise<any>}
- */
-const getFileData = async fileName => {
-    const filePath = path.join(__dirname, `./data/${fileName}`);
-    await fs.readFileSync(filePath)
-        .then(data => {
-            return JSON.parse(data)
-        })
-}
-
-/**
- * Set data to JSON file.
- * @param fileName
- * @param data
- * @returns {Promise<void>}
- */
-const setFileData = async (fileName, data) => {
-    const filePath = path.join(__dirname, `./data/${fileName}`);
-    const dataList = JSON.stringify(data, null, '  ');
-    await fs.writeFile(filePath, dataList);
-}
-
-module.exports = { getFileData, setFileData }
+module.exports = MonthToNumber;
